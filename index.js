6f607298f7d5c136d9149597666110a71a79be35
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-19 13:35:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const EasyCache = (()=>{
-  class _DummyCache{}
+  class _DummyCache{
+    async match(){}
+    async matchAll(){
+      return [];
+    }
+    async add(){}
+    async addAll(){}
+    asynv p
+  }
   return class EasyCache{}
 })();
