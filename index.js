87e1455a1191d6259391eafbea95d45758097d2d
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-19 14:08:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,40 @@ const EasyCache = (()=>{
     }
     async add(){}
     async addAll(){}
-    asynv p
+    async put(){}
+    async delete(){
+      return false;
+    }
+    async keys(){
+      return []
+    }
   }
-  return class EasyCache{}
+  const isPromise = x => x instanceof Promise || x?.constructor?.name == 'Promise' || typeof x?.then === 'function';
+  const $init = Symbol('*init');
+  const $getCache = Symbol('*getCache');
+  const $setCache = Symbol('*setCache');
+  return class EasyCache{
+    constructor(...args){
+      const $this = this;
+      $this[$init] = (async()=>{
+        try{
+          if(!args.length){
+            $this[$getCache] = caches.default ?? caches;
+            $this[$setCache] = caches.default ?? await caches.open('default');
+            return $this
+          }
+          if(args[0] == 'default'){
+            const $cache = caches.default ?? await caches.open('default');
+            $this[$getCache] = $cache;
+            $this[$setCache] = $cache;
+          }
+        }catch(e){
+          console.warn(e);
+          const dummy = new _DummyCache();
+          $this[$getCache] = dummy;
+          $this[$setCache] = dummy;
+        }
+      })();
+    }
+  }
 })();
