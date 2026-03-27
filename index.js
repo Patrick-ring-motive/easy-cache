eb412ebf2d90597f28383642b885f4da3f6e78c6
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-27 10:18:32 -0500

    style: js-beautify formatting

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const EasyCache = (()=>{
-  class _DummyCache{
-    async match(){}
-    async matchAll(){
+const EasyCache = (() => {
+  class _DummyCache {
+    async match() {}
+    async matchAll() {
       return [];
     }
-    async add(){}
-    async addAll(){}
-    async put(){}
-    async delete(){
+    async add() {}
+    async addAll() {}
+    async put() {}
+    async delete() {
       return false;
     }
-    async keys(){
+    async keys() {
       return []
     }
   }
@@ -18,27 +18,27 @@ const EasyCache = (()=>{
   const $init = Symbol('*init');
   const $getCache = Symbol('*getCache');
   const $setCache = Symbol('*setCache');
-  return class EasyCache{
-    constructor(...args){
+  return class EasyCache {
+    constructor(...args) {
       const $this = this;
-      $this[$init] = (async()=>{
-        try{
-          if(!args.length){
+      $this[$init] = (async () => {
+        try {
+          if (!args.length) {
             $this[$getCache] = caches.default ?? caches;
             $this[$setCache] = caches.default ?? await caches.open('default');
             return $this
           }
-          if(args[0] == 'default'){
+          if (args[0] == 'default') {
             const $cache = caches.default ?? await caches.open('default');
             $this[$getCache] = $cache;
             $this[$setCache] = $cache;
             return $this;
           }
-            const $cache = caches.default ?? await caches.open(String(args[0]));
-            $this[$getCache] = $cache;
-            $this[$setCache] = $cache;
-            return $this;
-        }catch(e){
+          const $cache = caches.default ?? await caches.open(String(args[0]));
+          $this[$getCache] = $cache;
+          $this[$setCache] = $cache;
+          return $this;
+        } catch (e) {
           console.warn(e);
           const dummy = new _DummyCache();
           $this[$getCache] = dummy;
@@ -47,95 +47,95 @@ const EasyCache = (()=>{
         }
       })();
     }
-    async match(...args){
-      try{
-        if(isPromise(this[$init])){
+    async match(...args) {
+      try {
+        if (isPromise(this[$init])) {
           this[$init] = await this[$init];
         }
         const url = String(args[0].url ?? args[0]);
         const options = args[1] ?? {};
         options.ignoreMethod ??= true;
         options.ignoreVary ??= true;
-        return (await this[$getCache].match(url,options))?.clone?.();
-      }catch(e){
+        return (await this[$getCache].match(url, options))?.clone?.();
+      } catch (e) {
         console.warn(e);
       }
     }
-    async matchAll(...args){
-      try{
-        if(isPromise(this[$init])){
+    async matchAll(...args) {
+      try {
+        if (isPromise(this[$init])) {
           this[$init] = await this[$init];
         }
         const url = String(args[0].url ?? args[0]);
         const options = args[1] ?? {};
         options.ignoreMethod ??= true;
         options.ignoreVary ??= true;
-        return (await (this[$getCache].matchAll??this[$setCache].matchAll)(url,options)).map(x=>x?.clone?.());
-      }catch(e){
+        return (await (this[$getCache].matchAll ?? this[$setCache].matchAll)(url, options)).map(x => x?.clone?.());
+      } catch (e) {
         console.warn(e);
         return [];
       }
     }
-    async add(...args){
-      try{
-        if(isPromise(this[$init])){
+    async add(...args) {
+      try {
+        if (isPromise(this[$init])) {
           this[$init] = await this[$init];
         }
         const url = String(args[0].url ?? args[0]);
         return await this[$setCache].add(url);
-      }catch(e){
+      } catch (e) {
         console.warn(e);
       }
     }
-    async addAll(...args){
-      try{
-        if(isPromise(this[$init])){
+    async addAll(...args) {
+      try {
+        if (isPromise(this[$init])) {
           this[$init] = await this[$init];
         }
-        const urls = args[0].map(x=>String(x?.url ?? x));
+        const urls = args[0].map(x => String(x?.url ?? x));
         return await this[$setCache].addAll(urls);
-      }catch(e){
+      } catch (e) {
         console.warn(e);
       }
     }
-    async put(req,res){
-      try{
-        if(isPromise(this[$init])){
+    async put(req, res) {
+      try {
+        if (isPromise(this[$init])) {
           this[$init] = await this[$init];
         }
         const url = String(req.url ?? req);
-        return await this[$setCache].put(url,res?.clone?.() ?? new Response(res?.body ?? res,res ?? {}));
-      }catch(e){
+        return await this[$setCache].put(url, res?.clone?.() ?? new Response(res?.body ?? res, res ?? {}));
+      } catch (e) {
         console.warn(e);
         return false;
       }
     }
-    async delete(...args){
-      try{
-        if(isPromise(this[$init])){
+    async delete(...args) {
+      try {
+        if (isPromise(this[$init])) {
           this[$init] = await this[$init];
         }
         const url = String(args[0].url ?? args[0]);
         const options = args[1] ?? {};
         options.ignoreMethod ??= true;
         options.ignoreVary ??= true;
-        return await this[$setCache].delete(url,options);
-      }catch(e){
+        return await this[$setCache].delete(url, options);
+      } catch (e) {
         console.warn(e);
         return false;
       }
     }
-    async keys(...args){
-      try{
-        if(isPromise(this[$init])){
+    async keys(...args) {
+      try {
+        if (isPromise(this[$init])) {
           this[$init] = await this[$init];
         }
         const url = String(args[0].url ?? args[0]);
         const options = args[1] ?? {};
         options.ignoreMethod ??= true;
         options.ignoreVary ??= true;
-        return (await this[$getCache].keys(url,options)).map(x=>x?.clone?.());
-      }catch(e){
+        return (await this[$getCache].keys(url, options)).map(x => x?.clone?.());
+      } catch (e) {
         console.warn(e);
         return [];
       }
